1a26f25fbd6a91857d4d612ee1044bbbc560400e
Isaac Gamus <igamus@gmail.com>
2023-08-08 20:29:57 -0400

    install fonts, favicon, create 404 page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,7 @@ import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
+import CustomError from "./components/CustomError";
 
 function App() {
   const dispatch = useDispatch();
@@ -24,6 +25,7 @@ function App() {
           <Route path="/signup">
             <SignupFormPage />
           </Route>
+          <CustomError />
         </Switch>
       )}
     </>
---
+++ react-app/src/components/CustomError/index.js
@@ -0,0 +1,17 @@
+import "./CustomError.css";
+
+function CustomError() {
+    return (
+        <div>
+            <h1>Wrong place</h1>
+            <p>Hey, so I understand that you're disappointed in me. I would be, too, except that I'm not here to experience this with you. As far as I know, this has never happened, and nobody has ever hit this page. Or ever will. Unless you contact me, I will never know that this page doesn't work, and I'll walk around smugly, self-assured I can build working websites.</p>
+            <p>But if you reach out to me, how would I know you're telling the truth? Why would I believe you?</p>
+            <p>Screencap? Ha! Photoshop, I'll say.</p>
+            <p>No, I would only believe a mentor. After all, only a mentor would be trusted to guide me and point out my faults with constructive intent.</p>
+            <p>The only logical way to communicate this issue to me is to hire and train me.</p>
+            <p>... over this bad code.</p>
+        </div>
+    );
+};
+
+export default CustomError;
