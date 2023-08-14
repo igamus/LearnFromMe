e298e177a627fe13b063bae2f98e8aaa3fe86588
Isaac Gamus <igamus@gmail.com>
2023-08-14 13:09:17 -0400

    Update cart routes to deal with edge cases

--- app/api/cart_routes.py
+++ app/api/cart_routes.py
@@ -13,12 +13,17 @@ def remove_course_from_cart(course_id):
     Remove course of specified id from cart
     """
     course = Course.query.get(course_id)
-    # make sure it's in cart first
+
+    if course is None:
+        return jsonify({"message": "Course does not exist"}), 404
+
+    if not current_user in course.users_with_course_in_cart:
+        return jsonify({"message": "Target course not in cart"}), 400
+
     try:
         course.users_with_course_in_cart.remove(current_user)
         db.session.commit()
-        # return updated cart...
-        return jsonify({"message": "Successfully removed course from cart"}), 200
+        return jsonify({"message": "Successfully removed course from cart"}), 200 # maybe return updated cart
     except Exception as error:
         db.session.rollback()
         return jsonify({"error":"An error occurred while updating the server"}), 500
@@ -31,12 +36,17 @@ def add_course_to_cart(course_id):
     Add course of specified id to cart
     """
     course = Course.query.get(course_id)
-    # see if already in cart
+
+    if course is None:
+        return jsonify({"message": "Course does not exist"}), 404
+
+    if current_user in course.users_with_course_in_cart:
+        return jsonify({"message": "Course already in cart"}), 400
+
     try:
         course.users_with_course_in_cart.append(current_user)
         db.session.commit()
-        # return the updated cart...
-        return jsonify({"addedCourse": course.to_dict()}), 200
+        return jsonify({"addedCourse": course.to_dict()}), 200 # maybe return updated cart
     except Exception as error:
         db.session.rollback()
         return jsonify({"error":"An error occurred while updating the server"}), 500
